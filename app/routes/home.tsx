19e562a0b559cbba1ad510acdabc7b6f21e8add8
zablink <zablinkapp@gmail.com>
2025-08-25 23:03:16 +0700

    test first time

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,7 +4,7 @@ import { Welcome } from "../welcome/welcome";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Welcome to React Router!ja" },
   ];
 }
 
