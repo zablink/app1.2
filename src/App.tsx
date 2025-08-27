f4cdb06b8356abe503923a01036ce0102101c392
zablink <zablinkapp@gmail.com>
2025-08-28 01:34:36 +0700

    reconfig to SPA

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+
+import React from "react";
+import Home from "./routes/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,13 @@
+// src/routes/Home.tsx
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-3xl font-bold">Welcome to SPA React + Vite!</h1>
+      <p className="mt-2">This setup works perfectly on Vercel.</p>
+    </div>
+  );
+};
+
+export default Home;
