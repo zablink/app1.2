0407017740146938f1e4dfdf197a1c243adb00a4
zablink <zablinkapp@gmail.com>
2025-08-28 01:05:13 +0700

    edit some configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import reactRouter from "@react-router/dev/vite"; // 👈 default import
 
 export default defineConfig({
   plugins: [
-    reactRouter(),   // 👈 ต้องเป็น function
     tsconfigPaths(),
     tailwindcss()
   ]
